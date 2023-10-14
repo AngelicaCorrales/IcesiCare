3949b2911292980a427c54ac89aff98c0a8bc6a9
Santiagokmids <sntgtrochez@gmail.com>
2023-10-14 16:42:13 -0500

    updated packages

--- app/src/main/java/icesi/edu/co/icesicare/MainActivity.kt
+++ app/src/main/java/icesi/edu/co/icesicare/MainActivity.kt
@@ -2,10 +2,16 @@ package icesi.edu.co.icesicare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import icesi.edu.co.icesicare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
