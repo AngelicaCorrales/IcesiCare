32d252d5f1ea45cde0509f4bd1572bc6e636a67b
Santiagokmids <sntgtrochez@gmail.com>
2023-10-15 17:39:18 -0500

    refactor: attribute student career changed

--- app/src/main/java/icesi/edu/co/icesicare/model/entity/Student.kt
+++ app/src/main/java/icesi/edu/co/icesicare/model/entity/Student.kt
@@ -3,7 +3,7 @@ package icesi.edu.co.icesicare.model.entity
 data class Student (
 
     var age : Int = 0,
-    var carrer : String = "",
+    var career : String = "",
     var code : String = "",
     var genre : String = "",
     var id : String = "",
