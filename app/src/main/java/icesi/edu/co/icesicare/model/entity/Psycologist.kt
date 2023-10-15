9a6a6695ad408b78fd5bfb3601a5a05311774975
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-10-15 13:25:17 -0500

    Filled PsychViewHolder and Psycologist data class

---
+++ app/src/main/java/icesi/edu/co/icesicare/model/entity/Psycologist.kt
@@ -0,0 +1,10 @@
+package icesi.edu.co.icesicare.model.entity
+
+data class Psychologist (
+    val id:String,
+    val name:String,
+    val genre:Char,
+    val email:String,
+    val profileImage: String,
+    val description:String
+)
