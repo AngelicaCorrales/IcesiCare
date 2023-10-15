270aa7a87980ba02de8c260f4b87c9e065e4d993
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-10-14 22:23:41 -0500

    Add base, blanck activity and fragments for making appointments

---
+++ app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
@@ -0,0 +1,12 @@
+package icesi.edu.co.icesicare.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import icesi.edu.co.icesicare.R
+
+class MakeAppointmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_appointment)
+    }
+}
