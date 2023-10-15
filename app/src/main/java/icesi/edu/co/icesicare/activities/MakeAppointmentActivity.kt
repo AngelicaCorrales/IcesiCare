1fc5b393cddb4e76dee0f86f2b474e2190b73e89
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-10-15 10:56:19 -0500

    Add base design for make appointment activity with nav bar and fragment container

--- app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
+++ app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
@@ -2,11 +2,15 @@ package icesi.edu.co.icesicare.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import icesi.edu.co.icesicare.R
+import icesi.edu.co.icesicare.databinding.ActivityMakeAppointmentBinding
 
 class MakeAppointmentActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMakeAppointmentBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_make_appointment)
+        setContentView(binding.root)
     }
 }
