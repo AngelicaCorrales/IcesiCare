00e3b80209dfd9a5af0a06c2927fafac2e8723c1
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-10-15 21:47:21 -0500

    action for button to make appointment added

--- app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
+++ app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
@@ -2,15 +2,25 @@ package icesi.edu.co.icesicare.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import icesi.edu.co.icesicare.R
 import icesi.edu.co.icesicare.databinding.ActivityMakeAppointmentBinding
+import icesi.edu.co.icesicare.view.fragments.MakeAppointmentPsychListFragment
 
 class MakeAppointmentActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMakeAppointmentBinding.inflate(layoutInflater)
     }
+
+    private val makeAppntPsychList=MakeAppointmentPsychListFragment.newInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        showFragment(makeAppntPsychList)
+
+    }
+    fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
     }
 }
--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragment.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragment.kt
@@ -1,10 +1,12 @@
 package icesi.edu.co.icesicare.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import icesi.edu.co.icesicare.activities.MakeAppointmentActivity
 import icesi.edu.co.icesicare.databinding.HomeFragmentBinding
 
 class HomeFragment: Fragment() {
@@ -15,6 +17,11 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: HomeFragmentBinding=HomeFragmentBinding.inflate(inflater,container,false)
+
+        binding.makeAppmBtn.setOnClickListener {
+            val intent=Intent(activity,MakeAppointmentActivity::class.java)
+            startActivity(intent)//if handling result needed, change to launch
+        }
 
         return binding.root
     }
