1da30d234d9f6abef003734d269289403898cf33
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-11-13 16:11:30 -0500

    CalendarViewHolder and Calendar adapter added

---
+++ app/src/main/java/icesi/edu/co/icesicare/view/adapters/CalendarAdapter.kt
@@ -0,0 +1,46 @@
+package icesi.edu.co.icesicare.view.adapters
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import icesi.edu.co.icesicare.R
+import icesi.edu.co.icesicare.util.CalendarUtils
+import icesi.edu.co.icesicare.viewholder.CalendarViewHolder
+import java.time.LocalDate
+
+
+class CalendarAdapter(
+    private val days: ArrayList<LocalDate?>,
+    private val onItemListener: OnItemListener
+) :
+    RecyclerView.Adapter<CalendarViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.calendar_cell, parent, false)
+        val layoutParams = view.layoutParams
+        if (days!!.size > 15) //month view
+            layoutParams.height = (parent.height * 0.166666666).toInt() else  // week view
+            layoutParams.height = parent.height
+        return CalendarViewHolder(view, onItemListener, days)
+    }
+
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        val date = days?.get(position)
+        if (date == null) holder.dayOfMonth.text = "" else {
+            holder.dayOfMonth.text = date.dayOfMonth.toString()
+            if (date == CalendarUtils.selectedDate) holder.parentView.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return days!!.size
+    }
+
+    interface OnItemListener {
+        fun onItemClick(position: Int, date: LocalDate?)
+    }
+}
+
+
