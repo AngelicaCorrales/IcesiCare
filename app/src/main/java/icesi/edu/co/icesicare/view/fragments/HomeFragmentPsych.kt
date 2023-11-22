a2232b4a25b48cb5fde7a42cd66748b0382520ad
Santiagokmids <sntgtrochez@gmail.com>
2023-11-22 17:28:08 -0500

    feat: welcome message custom

--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragmentPsych.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragmentPsych.kt
@@ -6,12 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import icesi.edu.co.icesicare.activities.AcceptAppointmentActivity
 import icesi.edu.co.icesicare.databinding.FragmentHomePsychBinding
+import icesi.edu.co.icesicare.viewmodel.PsychologistViewModel
 
 class HomeFragmentPsych : Fragment() {
 
     private lateinit var binding: FragmentHomePsychBinding
+    private val viewModel : PsychologistViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,19 @@ class HomeFragmentPsych : Fragment() {
     ): View? {
 
         binding = FragmentHomePsychBinding.inflate(inflater,container,false)
+
+        viewModel.getPsychologist(Firebase.auth.currentUser!!.uid)
+        viewModel.psychologistLV.observe(viewLifecycleOwner){
+            val welcomeUser = "Hola, "+it.name
+            binding.helloTv.text = welcomeUser
+
+            if (it.genre == "Femenino"){
+                binding.welcomeTV.text = "Bienvenida"
+
+            }else if (it.genre == "Otro"){
+                binding.welcomeTV.text = "Bienvenide"
+            }
+        }
 
         binding.acceptApptBtn.setOnClickListener {
             val intent= Intent(activity, AcceptAppointmentActivity::class.java)
--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragmentStud.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/HomeFragmentStud.kt
@@ -6,12 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import icesi.edu.co.icesicare.activities.MakeAppointmentActivity
 import icesi.edu.co.icesicare.databinding.FragmentHomeStudBinding
+import icesi.edu.co.icesicare.viewmodel.StudentProfileViewModel
 
 class HomeFragmentStud : Fragment() {
 
     private lateinit var binding: FragmentHomeStudBinding
+    private val viewModel : StudentProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,19 @@ class HomeFragmentStud : Fragment() {
     ): View? {
 
         binding = FragmentHomeStudBinding.inflate(inflater,container,false)
+
+        viewModel.getStudent(Firebase.auth.currentUser!!.uid)
+        viewModel.studentLV.observe(viewLifecycleOwner){
+            val welcomeUser = "Hola, "+it.name
+            binding.helloTv.text = welcomeUser
+
+            if (it.genre == "Femenino"){
+                binding.welcomeTV.text = "Bienvenida"
+
+            }else if (it.genre == "Otro"){
+                binding.welcomeTV.text = "Bienvenide"
+            }
+        }
 
         binding.makeAppmBtn.setOnClickListener {
             val intent= Intent(activity, MakeAppointmentActivity::class.java)
