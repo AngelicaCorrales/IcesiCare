9c555c8a5b1c0e194f2bc5e3615275231b9df025
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-11-23 18:57:23 -0500

    Add search psychologist functionality when making appointments, remove unnused buttons from MakeAppointmentActivity layout

--- app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
+++ app/src/main/java/icesi/edu/co/icesicare/activities/MakeAppointmentActivity.kt
@@ -1,14 +1,18 @@
 package icesi.edu.co.icesicare.activities
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import icesi.edu.co.icesicare.R
 import icesi.edu.co.icesicare.databinding.ActivityMakeAppointmentBinding
 import icesi.edu.co.icesicare.model.entity.Psychologist
 import icesi.edu.co.icesicare.view.fragments.MakeAppointmentPsychDetailFragment
 import icesi.edu.co.icesicare.view.fragments.MakeAppointmentPsychListFragment
+
 
 class MakeAppointmentActivity : AppCompatActivity() {
 
@@ -23,6 +27,53 @@ class MakeAppointmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         showFragmentPsychList()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        binding.searchBtn.setOnClickListener{
+            binding.psychTV.visibility = View.GONE
+            binding.backBtn.visibility = View.GONE
+            binding.searchBtn.visibility = View.GONE
+            binding.cancelSearchBtn.visibility = View.VISIBLE
+            binding.searchView.visibility = View.VISIBLE
+            binding.searchView.requestFocus()
+
+            imm.showSoftInput(binding.searchView, 0)
+        }
+
+        //Only works when query is not empty due to null check in the implementation of SearchView
+        binding.searchView.setOnSearchClickListener{
+            imm.hideSoftInputFromWindow(binding.searchView.windowToken,0)
+        }
+
+        binding.cancelSearchBtn.setOnClickListener{
+            binding.psychTV.visibility = View.VISIBLE
+            binding.backBtn.visibility = View.VISIBLE
+            binding.searchBtn.visibility = View.VISIBLE
+            binding.cancelSearchBtn.visibility = View.GONE
+            binding.searchView.visibility = View.GONE
+            binding.searchView.setQuery("",false)
+            makeAppntPsychListFragment.clearFilter()
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    makeAppntPsychListFragment.filterPsychsByName(newText)
+                    return true
+                }
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    makeAppntPsychListFragment.filterPsychsByName(query)
+                    return true
+                }
+                return false
+            }
+        })
     }
 
     fun showFragmentPsychList(){
@@ -31,8 +82,6 @@ class MakeAppointmentActivity : AppCompatActivity() {
 
         binding.psychTV.visibility = View.VISIBLE
         binding.searchBtn.visibility = View.VISIBLE
-        binding.filterBtn.visibility = View.VISIBLE
-        binding.helpBtn.visibility = View.VISIBLE
 
         binding.backBtn.setOnClickListener{
             val intent= Intent(this,StudentMainActivity::class.java)
@@ -55,8 +104,6 @@ class MakeAppointmentActivity : AppCompatActivity() {
         makeAppntPsychDetailFragment.psych = psych
         binding.psychTV.visibility = View.GONE
         binding.searchBtn.visibility = View.GONE
-        binding.filterBtn.visibility = View.GONE
-        binding.helpBtn.visibility = View.GONE
 
         binding.backBtn.setOnClickListener{
             showFragmentPsychList()
