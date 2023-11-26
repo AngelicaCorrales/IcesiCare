6cd369a9d545faed0d587a15861e852efdc07e87
AngelicaCorrales <72984945+AngelicaCorrales@users.noreply.github.com>
2023-11-25 21:10:07 -0500

    Info of psychologist shown in make appo detail

--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/MakeAppointmentPsychDetailFragment.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/MakeAppointmentPsychDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import icesi.edu.co.icesicare.databinding.FragmentMakeAppointmentPsychDetailBinding
 import icesi.edu.co.icesicare.model.entity.Psychologist
 
@@ -22,10 +23,20 @@ class MakeAppointmentPsychDetailFragment : Fragment() {
     ): View? {
 
         binding = FragmentMakeAppointmentPsychDetailBinding.inflate(inflater,container,false)
-
+        loadPsychDetail()
         loadWeeklyCalendar()
 
         return binding.root
+    }
+
+    fun loadPsychDetail(){
+        binding.namePsycTV.text = psych?.name
+        binding.descriptionPsycTV.text = psych?.description
+
+       if(psych?.profileImageURL != ""){
+            Glide.with(this).load(psych?.profileImageURL).into(binding.imagePsycIV)
+        }
+
     }
 
     fun loadWeeklyCalendar() {
