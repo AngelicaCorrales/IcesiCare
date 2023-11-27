95f88034c94ec50fa0f7008cc567f23674bc2db7
SeanQO <seanquintero3@gmail.com>
2023-11-27 00:58:14 -0500

    schedule design finished

--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyScheduleFragment.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyScheduleFragment.kt
@@ -2,6 +2,7 @@ package icesi.edu.co.icesicare.view.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.format.DateFormat.is24HourFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import icesi.edu.co.icesicare.R
 import icesi.edu.co.icesicare.activities.PsyProfileActivity
 import icesi.edu.co.icesicare.databinding.FragmentEditPsyScheduleBinding
@@ -22,14 +25,15 @@ class EditPsyScheduleFragment  : Fragment() {
     private var _binding: FragmentEditPsyScheduleBinding? = null
     private val binding get() = _binding!!
 
-    private val items = listOf("Lunes", "Martes", "Miércoles", "Jueves")
+    private val items = listOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes")
     override fun onResume() {
         super.onResume()
         val adapter = ArrayAdapter(requireContext(), R.layout.drop_down_days_item, items)
         binding.daysInput.setAdapter(adapter)
         binding.daysInput.setOnItemClickListener { adapterView, _, position, _ ->
             val selectedDay = adapterView.getItemAtPosition(position).toString()
-            binding.textToShow.text = selectedDay
+            binding.fromTimeTextView.text = selectedDay
+            binding.toTimeTextView.text = selectedDay
         }
     }
     override fun onCreateView(
@@ -52,12 +56,49 @@ class EditPsyScheduleFragment  : Fragment() {
 
         fragmentActivity.showLoading(false)
 
+        binding.editFromTimeBtn.setOnClickListener {
+            fromOpenTimePicker()
+        }
+
+        binding.editToTimeBtn.setOnClickListener {
+            toOpenTimePicker()
+        }
+
+        binding.saveScheduleChangesBtn.setOnClickListener {
+            saveSchedule()
+        }
+
         binding.backBtn.setOnClickListener {
             fragmentActivity.showEditProfileFragment()
         }
 
         return binding.root
     }
+
+    private fun fromOpenTimePicker(){
+        openTimePicker()
+
+    }
+    private fun toOpenTimePicker(){
+        openTimePicker()
+
+    }
+    private fun openTimePicker(){
+        val isSys24hFormat = is24HourFormat(requireContext())
+        val clockFormat = if(isSys24hFormat) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H
+
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(clockFormat)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Selecciona tu horario")
+            .build()
+        picker.show(childFragmentManager, "TAG")
+    }
+    private fun saveSchedule(){
+
+    }
+
     companion object {
         @JvmStatic
         fun newInstance(): EditPsyScheduleFragment {
