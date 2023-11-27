01f723c3accf53888ee76cc06ef280129aa7b415
SeanQO <seanquintero3@gmail.com>
2023-11-26 22:35:04 -0500

    added schedule fixes and adaptations

--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyScheduleFragment.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyScheduleFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import icesi.edu.co.icesicare.R
 import icesi.edu.co.icesicare.activities.PsyProfileActivity
 import icesi.edu.co.icesicare.databinding.FragmentEditPsyScheduleBinding
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +19,19 @@ import kotlinx.coroutines.withContext
 
 class EditPsyScheduleFragment  : Fragment() {
     private lateinit var fragmentActivity: PsyProfileActivity
+    private var _binding: FragmentEditPsyScheduleBinding? = null
+    private val binding get() = _binding!!
+
+    private val items = listOf("Lunes", "Martes", "Miércoles", "Jueves")
+    override fun onResume() {
+        super.onResume()
+        val adapter = ArrayAdapter(requireContext(), R.layout.drop_down_days_item, items)
+        binding.daysInput.setAdapter(adapter)
+        binding.daysInput.setOnItemClickListener { adapterView, _, position, _ ->
+            val selectedDay = adapterView.getItemAtPosition(position).toString()
+            binding.textToShow.text = selectedDay
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,7 +39,7 @@ class EditPsyScheduleFragment  : Fragment() {
     ): View? {
         fragmentActivity = activity as PsyProfileActivity
         fragmentActivity.showLoading(true)
-        val binding: FragmentEditPsyScheduleBinding = FragmentEditPsyScheduleBinding.inflate(inflater,container,false)
+        _binding = FragmentEditPsyScheduleBinding.inflate(inflater,container,false)
 
         binding.saveScheduleChangesBtn.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
