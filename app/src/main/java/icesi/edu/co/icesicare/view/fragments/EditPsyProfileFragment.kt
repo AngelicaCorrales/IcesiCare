c8eea8feac163a3582fa79b93c5f3bb7f715c31e
SeanQO <seanquintero3@gmail.com>
2023-11-29 22:36:31 -0500

    camera and gallery updated

--- app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyProfileFragment.kt
+++ app/src/main/java/icesi/edu/co/icesicare/view/fragments/EditPsyProfileFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
@@ -18,15 +20,23 @@ import icesi.edu.co.icesicare.activities.PsyProfileActivity
 import icesi.edu.co.icesicare.databinding.FragmentEditpsyprofileBinding
 import icesi.edu.co.icesicare.model.entity.Psychologist
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.core.content.FileProvider
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import icesi.edu.co.icesicare.model.repository.PsychRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.UUID
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 
 class EditPsyProfileFragment : Fragment() {
 
@@ -34,7 +44,15 @@ class EditPsyProfileFragment : Fragment() {
     private lateinit var binding: FragmentEditpsyprofileBinding
     private lateinit var galleryLauncher : ActivityResultLauncher<Intent>
     private lateinit var newImageURIStr: String
-
+    private lateinit var imgUrl: Uri
+    private lateinit var cameraLauncher: ActivityResultLauncher<Uri>
+    private val requestCameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            launchCamera()
+        } else {
+            Toast.makeText(requireContext(), "Camera permission is required", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -89,8 +107,11 @@ class EditPsyProfileFragment : Fragment() {
         }
 
         binding.selectPictureFromCamBtn.setOnClickListener {
-
-
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            } else {
+                launchCamera()
+            }
         }
 
         binding.saveChangesBtn.setOnClickListener {
@@ -106,6 +127,43 @@ class EditPsyProfileFragment : Fragment() {
         return binding.root
     }
 
+    private fun createImgFileUri(): Uri {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = File(
+            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+            "/Android/data/icesi.edu.co.icesicare/files"
+        )
+        if (!storageDir.exists()) {
+            storageDir.mkdirs()
+        }
+
+        val image = File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
+
+        return FileProvider.getUriForFile(
+            requireContext(),
+            "icesi.edu.co.icesicare",
+            image
+        )
+    }
+
+    private fun launchCamera() {
+        imgUrl = createImgFileUri()
+        cameraLauncher.launch(imgUrl)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                newImageURIStr = UUID.randomUUID().toString()
+                Firebase.storage.reference.child("users").child("profileImages").child(newImageURIStr).putFile(imgUrl)
+            }
+        }
+    }
     fun onGalleryResult (result: ActivityResult){
         if (result.resultCode == RESULT_OK){
             val uri = result.data?.data
@@ -115,7 +173,6 @@ class EditPsyProfileFragment : Fragment() {
             Firebase.storage.reference.child("users").child("profileImages").child(newImageURIStr).putFile(uri!!)
         }
     }
-
 
     private fun String.toEditable(): Editable =  Editable.Factory.getInstance().newEditable(this)
     private fun saveChanges(){
@@ -140,9 +197,11 @@ class EditPsyProfileFragment : Fragment() {
         }
     }
     companion object {
+        private const val CAMERA_REQUEST_CODE = 100
         @JvmStatic
         fun newInstance(): EditPsyProfileFragment {
             return EditPsyProfileFragment()
         }
     }
+
 }
