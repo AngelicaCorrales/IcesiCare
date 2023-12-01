517c677d4fbcf16c24f9546025f055d8f81b058e
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-12-01 01:39:56 -0500

    Add initialize chat documents in firebase on accept appointment

---
+++ app/src/main/java/icesi/edu/co/icesicare/model/entity/ChatData.kt
@@ -0,0 +1,5 @@
+package icesi.edu.co.icesicare.model.entity
+data class ChatData(
+    val id:String = "",
+    val contactId:String = ""
+)
---
+++ app/src/main/java/icesi/edu/co/icesicare/model/repository/ChatRepository.kt
@@ -0,0 +1,54 @@
+package icesi.edu.co.icesicare.model.repository
+
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import icesi.edu.co.icesicare.model.entity.Chat
+import icesi.edu.co.icesicare.model.entity.ChatData
+import kotlinx.coroutines.tasks.await
+import java.util.UUID
+
+class ChatRepository {
+
+    /**
+     * This function assumes the student and psychologist exist
+     */
+    suspend fun initializeChat(studId: String, psychId: String) {
+
+        //Check if both dont have a chat, if they have, dont create new one
+
+        val result1 = Firebase.firestore.collection("students")
+            .document(studId)
+            .collection("chats")
+            .whereEqualTo("contactId",psychId)
+            .get().await()
+
+        if (!result1.isEmpty) return
+
+        val result2 = Firebase.firestore.collection("students")
+            .document(psychId)
+            .collection("chats")
+            .whereEqualTo("contactId",studId)
+            .get().await()
+        if (!result2.isEmpty) return
+
+        val chatId = UUID.randomUUID().toString()
+
+        Firebase.firestore.collection("chats")
+            .document(chatId).set(Chat(id=chatId)).await()
+
+        Firebase.firestore.collection("students")
+            .document(studId)
+            .collection("chats")
+            .document(chatId)
+            .set(ChatData(chatId,psychId))
+            .await()
+
+        Firebase.firestore.collection("psychologists")
+            .document(psychId)
+            .collection("chats")
+            .document(chatId)
+            .set(ChatData(chatId,studId))
+            .await()
+    }
+}
